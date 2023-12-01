682a96baa2e98929fb69288345383f3aef80afe1
ë닉네임 <github >
2023-12-01 18:16:11 +0900

    ryuhanseon

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!!!0000");
     }
 }
